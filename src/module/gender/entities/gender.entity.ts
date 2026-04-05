f198a284919a03972678d6136a7cfc13f863620a
Chow <chowduarte@gmail.com>
2026-04-04 21:29:10 -0600

    feat: define Gender entity with properties and decorators

--- src/module/gender/entities/gender.entity.ts
+++ src/module/gender/entities/gender.entity.ts
@@ -1,1 +1,21 @@
-export class Gender {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class Gender {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @CreateDateColumn()
+  updated_at: Date;
+}
