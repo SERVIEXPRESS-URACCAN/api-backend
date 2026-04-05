29c45411d7fd5bca726538d0c7bce8fd2be8edf4
Chow <chowduarte@gmail.com>
2026-04-05 16:30:04 -0600

    fix: remove id property from CreateGenderDto because it is not necessary

--- src/module/gender/dto/create-gender.dto.ts
+++ src/module/gender/dto/create-gender.dto.ts
@@ -1,9 +1,5 @@
-import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 export class CreateGenderDto {
-  @IsNumber()
-  @IsNotEmpty()
-  id?: number;
-
   @IsString()
   @IsNotEmpty()
   name: string;
