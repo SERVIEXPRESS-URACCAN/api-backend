7270a7486a4fdeeb17c14e88b40ad6f351a700be
Chow <chowduarte@gmail.com>
2026-04-04 20:23:43 -0600

    feat: add gender module with service, controller, and DTOs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GenderModule } from './module/gender/gender.module';
 
 @Module({
-  imports: [],
+  imports: [GenderModule],
   controllers: [AppController],
   providers: [AppService],
 })
