f5a0b4d3900531edce0c5573435013e1b54b851e
Chow <chowduarte@gmail.com>
2026-04-05 21:14:46 -0600

    fix: add optional id property with validation to CreateGenderDto

--- src/module/gender/dto/create-gender.dto.ts
+++ src/module/gender/dto/create-gender.dto.ts
@@ -1,5 +1,9 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsInt, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 export class CreateGenderDto {
+  @IsInt()
+  @IsOptional()
+  id?: number;
+
   @IsString()
   @IsNotEmpty()
   name: string;
