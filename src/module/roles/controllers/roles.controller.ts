d91fb2dc6726270cefadb3caf43aa78e8f26f6ab
katia-silva09 <katiajuliethsilvareyes09@gmail.com>
2026-04-09 17:00:23 -0600

    feat: implement roles management with CRUD operations; add update and delete functionality

--- src/module/roles/controllers/roles.controller.ts
+++ src/module/roles/controllers/roles.controller.ts
@@ -1,6 +1,16 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { RolesService } from '../services/roles.service';
 import { CreateRolesDto } from '../dto/create-roles.dto';
+import { UpdateRolesDto } from '../dto/update-roles.dto';
 
 @Controller('roles')
 export class RolesController {
@@ -12,12 +22,25 @@ export class RolesController {
   }
 
   @Get(':id')
-  async getOne(@Param('id') id: number) {
+  getOne(@Param('id') id: number) {
     return this.rolesService.getOne(id);
   }
 
   @Post()
   async create(@Body() createRolesDto: CreateRolesDto) {
     return this.rolesService.create(createRolesDto);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateRolesDto: UpdateRolesDto,
+  ) {
+    return this.rolesService.update(id, updateRolesDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.rolesService.remove(id);
   }
 }
