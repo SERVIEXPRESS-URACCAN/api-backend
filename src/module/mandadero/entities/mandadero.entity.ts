263cacab2cbec2033a864e0e8c51f459dce76002
Chow <chowduarte@gmail.com>
2026-04-09 16:56:35 -0600

    feat: refactor MandaderoController and MandaderoService for availability and activation updates

--- src/module/mandadero/entities/mandadero.entity.ts
+++ src/module/mandadero/entities/mandadero.entity.ts
@@ -12,8 +12,11 @@ export class Mandadero {
   @PrimaryGeneratedColumn('increment')
   id: number;
 
-  @Column()
+  @Column({ default: false })
   available: boolean;
+
+  @Column({ default: true })
+  isActive: boolean;
 
   @OneToOne(() => User, (user) => user.mandadero)
   @JoinColumn({ name: 'user_id', referencedColumnName: 'id' })
