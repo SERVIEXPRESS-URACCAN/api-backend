bd6b1aec74372b0996bdfbd7b3d90f55ca193ce9
Chow <chowduarte@gmail.com>
2026-04-09 07:22:49 -0600

    feat: add Mandadero entity with user relationship and availability field

---
+++ src/module/mandadero/entities/mandadero.entity.ts
@@ -0,0 +1,24 @@
+import { User } from 'src/module/users/entities/user.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class Mandadero {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column()
+  available: boolean;
+
+  @OneToOne(() => User, (user) => user.mandadero)
+  @JoinColumn({ name: 'user_id', referencedColumnName: 'id' })
+  user: User;
+
+  //   @Column({ nullable: false })
+  //   imageIdentification: string;
+}
