d5288aab9c36093b90f56f43d5d8f8a5866701a1
katia-silva09 <katiajuliethsilvareyes09@gmail.com>
2026-04-12 16:21:46 -0600

    feat: implement JWT authentication in auth module; add jwtConstants and update login functionality

--- src/module/auth/auth.service.ts
+++ src/module/auth/auth.service.ts
@@ -2,10 +2,14 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/service/users.service';
 import { LoginDto } from './dto/login.dto';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
   async login({ email, password }: LoginDto) {
     const user = await this.usersService.findOneByEmail(email);
 
@@ -17,8 +21,12 @@ export class AuthService {
     if (!isPasswordValid) {
       throw new UnauthorizedException('Invalid password');
     }
+
+    const payload = { email: user.email };
+    const token = await this.jwtService.signAsync(payload);
     return {
       message: 'Login successful',
+      token,
     };
   }
 }
