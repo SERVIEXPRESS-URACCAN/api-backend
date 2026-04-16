0e6946f6166edf8b7aa56d27fcc578218313e5f5
Bryan18-Alvarado <bryanalvarado1904@gmail.com>
2026-04-16 09:31:33 -0600

    feat: create structure module

---
+++ src/owner/controller/owner.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CreateOwnerDto } from '../dto/create-owner.dto';
+import { UpdateOwnerDto } from '../dto/update-owner.dto';
+import { OwnerService } from '../service/owner.service';
+
+@Controller('owner')
+export class OwnerController {
+  constructor(private readonly ownerService: OwnerService) {}
+
+  @Post()
+  create(@Body() createOwnerDto: CreateOwnerDto) {
+    return this.ownerService.create(createOwnerDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.ownerService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.ownerService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateOwnerDto: UpdateOwnerDto) {
+    return this.ownerService.update(+id, updateOwnerDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.ownerService.remove(+id);
+  }
+}
