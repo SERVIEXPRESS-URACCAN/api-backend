20a1cd6b86e08ee30fdff89eec8b900363e5dfc2
Bryan18-Alvarado <bryanalvarado1904@gmail.com>
2026-04-16 10:56:08 -0600

    fix: reorganize imports and set global API prefix

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
