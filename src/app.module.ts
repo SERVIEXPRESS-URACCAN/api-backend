b9eeb6db8f9984c975d8ee0309ea9b48a44a5bf2
katia-silva09 <katiajuliethsilvareyes09@gmail.com>
2026-04-17 15:14:09 -0600

    refactor: reorganize module imports and update paths for improved structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './module/auth/auth.module';
+import { AuthModule } from './auth/auth.module';
 import { CategoriesBusinessModule } from './module/categories-business/categories-business.module';
 import { CategoriesProductsModule } from './module/categories-products/categories-products.module';
 import { GenderModule } from './module/gender/gender.module';
--- src/module/auth/auth.module.ts
+++ src/module/auth/auth.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { UsersModule } from '../users/users.module';
 import { JwtModule, JwtModuleOptions } from '@nestjs/jwt';
 import { AuthGuard } from './guard/auth.guard';
 import { RolesGuard } from './guard/roles.guard';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { StringValue } from 'ms';
 import { JwtStrategy } from './strategy/jwt.strategy';
+import { UsersModule } from '../users/users.module';
 
 @Module({
   imports: [
