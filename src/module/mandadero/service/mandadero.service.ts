19ef4b166e00281c8dcda49dbd5493e02b1c3d46
Chow <chowduarte@gmail.com>
2026-04-17 19:48:19 -0600

    fix: enhance MandaderoService with user validation and file management for create and update operations

--- src/module/mandadero/service/mandadero.service.ts
+++ src/module/mandadero/service/mandadero.service.ts
@@ -7,22 +7,28 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Mandadero } from '../entities/mandadero.entity';
 import { CreateMandaderoDto } from '../dto/create-mandadero.dto';
-
+import { User } from 'src/module/users/entities/user.entity';
+import * as path from 'path';
+import * as fs from 'fs';
 @Injectable()
 export class MandaderoService {
   constructor(
     @InjectRepository(Mandadero)
     private readonly mandaderoRepository: Repository<Mandadero>,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
   ) {}
 
   async updateAvailability(id: number, available: boolean) {
     const mandadero = await this.mandaderoRepository.findOneBy({ id });
     if (!mandadero) {
-      throw new NotFoundException('Mandadero no encontrado');
+      throw new NotFoundException('Mandadero not found');
     }
 
     if (!mandadero.isActive) {
-      throw new BadRequestException('El perfil no está activo');
+      throw new BadRequestException(
+        'Cannot change availability of an inactive mandadero',
+      );
     }
 
     mandadero.available = available;
@@ -33,7 +39,7 @@ export class MandaderoService {
   async updateActive(id: number, isActive: boolean) {
     const mandadero = await this.mandaderoRepository.findOneBy({ id });
     if (!mandadero) {
-      throw new NotFoundException('Mandadero no encontrado');
+      throw new NotFoundException('Mandadero not found');
     }
 
     mandadero.isActive = isActive;
@@ -41,27 +47,87 @@ export class MandaderoService {
     return this.mandaderoRepository.save(mandadero);
   }
 
-  async create(CreateMandaderoDto: CreateMandaderoDto) {
-    const mandadero = this.mandaderoRepository.create({
-      ...CreateMandaderoDto,
-      user: { id: CreateMandaderoDto.user },
+  async create(dto: CreateMandaderoDto) {
+    const user = await this.userRepository.findOne({
+      where: { id: dto.user },
     });
 
-    return await this.mandaderoRepository.save(mandadero);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    const mandadero = this.mandaderoRepository.create({
+      available: dto.available,
+      isActive: true,
+      user,
+    });
+
+    return this.mandaderoRepository.save(mandadero);
   }
 
   async findAll() {
-    return this.mandaderoRepository.find({ relations: ['user'] });
+    return this.mandaderoRepository.find({
+      relations: ['user', 'user.role', 'motorcycle'],
+    });
   }
 
   async findOne(id: number) {
     const mandadero = await this.mandaderoRepository.findOne({
       where: { id },
-      relations: ['user'],
+      relations: ['user', 'user.role', 'motorcycle'],
     });
 
-    if (!mandadero) throw new NotFoundException('Mandadero no encontrado');
+    if (!mandadero) throw new NotFoundException('Mandadero not found');
 
     return mandadero;
+  }
+  async remove(id: number) {
+    const mandadero = await this.mandaderoRepository.findOneBy({ id });
+    if (!mandadero) {
+      throw new NotFoundException('Mandadero not found');
+    }
+
+    return this.mandaderoRepository.softDelete(id);
+  }
+
+  async createWithFile(body: CreateMandaderoDto, file: Express.Multer.File) {
+    const user = await this.userRepository.findOne({
+      where: { id: body.user },
+    });
+
+    if (!user) {
+      throw new NotFoundException('User no encontrado');
+    }
+
+    const mandadero = this.mandaderoRepository.create({
+      available: body.available,
+      isActive: true,
+      user,
+      imageIdentification: file?.filename,
+    });
+
+    return this.mandaderoRepository.save(mandadero);
+  }
+
+  async updateFile(id: number, file: Express.Multer.File) {
+    const mandadero = await this.mandaderoRepository.findOne({ where: { id } });
+    if (!mandadero) {
+      throw new NotFoundException('Mandadero not found');
+    }
+
+    //boorra la vieja
+    if (mandadero.imageIdentification) {
+      const oldFile = path.join(
+        process.cwd(),
+        'uploads/mandaderos',
+        mandadero.imageIdentification,
+      );
+      if (fs.existsSync(oldFile)) {
+        fs.unlinkSync(oldFile);
+      }
+    }
+    mandadero.imageIdentification = file.filename;
+
+    return this.mandaderoRepository.save(mandadero);
   }
 }
