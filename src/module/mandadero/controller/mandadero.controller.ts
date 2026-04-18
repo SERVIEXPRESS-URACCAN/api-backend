fb9b8cd58ec9e1d4a272db87a8b61256e3b29d51
Chow <chowduarte@gmail.com>
2026-04-17 19:47:01 -0600

    feat: implement file upload and management in MandaderoController

--- src/module/mandadero/controller/mandadero.controller.ts
+++ src/module/mandadero/controller/mandadero.controller.ts
@@ -1,18 +1,49 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   Param,
   ParseIntPipe,
   Patch,
   Post,
+  UploadedFile,
+  UseInterceptors,
 } from '@nestjs/common';
 import { MandaderoService } from '../service/mandadero.service';
 import { CreateMandaderoDto } from '../dto/create-mandadero.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
+
+type UploadedFile = {
+  path: string;
+  mimetype: string;
+  size: number;
+};
 
 @Controller('mandadero')
 export class MandaderoController {
   constructor(private readonly MandaderoService: MandaderoService) {}
+
+  @Post()
+  @UseInterceptors(
+    FileInterceptor('imageIdentification', {
+      storage: diskStorage({
+        destination: './uploads/mandaderos',
+        filename: (req, file, cb) => {
+          const unique = Date.now() + '-' + Math.round(Math.random() * 1e9);
+          cb(null, unique + extname(file.originalname));
+        },
+      }),
+    }),
+  )
+  create(
+    @Body() body: CreateMandaderoDto,
+    @UploadedFile() file: Express.Multer.File,
+  ) {
+    return this.MandaderoService.createWithFile(body, file);
+  }
 
   @Patch(':id')
   updateAvailability(
@@ -30,19 +61,6 @@ export class MandaderoController {
     return this.MandaderoService.updateActive(id, body.isActive);
   }
 
-  @Post()
-  async create(@Body() createMandaderoDto: CreateMandaderoDto) {
-    const mandadero = await this.MandaderoService.create({
-      ...createMandaderoDto,
-      user: createMandaderoDto.user,
-    });
-
-    return {
-      data: mandadero,
-      message: 'Mandadero creado con exito',
-    };
-  }
-
   @Get()
   findAll() {
     return this.MandaderoService.findAll();
@@ -50,5 +68,28 @@ export class MandaderoController {
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.MandaderoService.findOne(id);
+  }
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.MandaderoService.remove(id);
+  }
+
+  @Patch(':id/file')
+  @UseInterceptors(
+    FileInterceptor('imageIdentification', {
+      storage: diskStorage({
+        destination: './uploads/mandaderos',
+        filename: (req, file, cb) => {
+          const unique = Date.now() + '-' + Math.round(Math.random() * 1e9);
+          cb(null, unique + extname(file.originalname));
+        },
+      }),
+    }),
+  )
+  updateFile(
+    @Param('id', ParseIntPipe) id: number,
+    @UploadedFile() file: Express.Multer.File,
+  ) {
+    return this.MandaderoService.updateFile(id, file);
   }
 }
