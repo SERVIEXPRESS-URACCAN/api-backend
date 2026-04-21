09711a737b78f446ecd66cad9ddb2a7da1aa3d5e
Bryan18-Alvarado <bryanalvarado1904@gmail.com>
2026-04-21 13:33:27 -0600

    feat: agregar controlador Business y entidad CategoriesBusiness con relaciones

---
+++ src/module/business/controller/business.controller.ts
@@ -0,0 +1,55 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CreateBusinessDto } from '../dto/create-business.dto';
+import { UpdateBusinessDto } from '../dto/update-business.dto';
+import { BusinessService } from '../service/business.service';
+
+@Controller('business')
+export class BusinessController {
+  constructor(private readonly businessService: BusinessService) {}
+
+  @Post()
+  async create(@Body() createBusinessDto: CreateBusinessDto) {
+    const business = await this.businessService.create({
+      ...createBusinessDto,
+      businessCategories: createBusinessDto.businessCategories,
+      user: createBusinessDto.user,
+    });
+
+    return {
+      data: business,
+      message: 'Business creado con exito',
+    };
+  }
+
+  @Get()
+  findAll() {
+    return this.businessService.findAll();
+  }
+
+  @Get(':id')
+  getOne(@Param('id', ParseIntPipe) id: number) {
+    return this.businessService.getOne(id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateBusinessDto: UpdateBusinessDto,
+  ) {
+    return this.businessService.update(+id, updateBusinessDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.businessService.remove(+id);
+  }
+}
