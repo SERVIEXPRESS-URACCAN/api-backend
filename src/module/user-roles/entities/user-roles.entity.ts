16d0d8a81630c54f02c8444791e6c90cb33e46c0
katia-silva09 <katiajuliethsilvareyes09@gmail.com>
2026-04-21 22:49:46 -0600

    feat: add UserRoles entity and module, and establish relationship with User entity

---
+++ src/module/user-roles/entities/user-roles.entity.ts
@@ -0,0 +1,31 @@
+import { Roles } from 'src/module/roles/entities/roles.entity';
+import { User } from 'src/module/users/entities/user.entity';
+import {
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class UserRoles {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => User, (user) => user.userRoles)
+  user: User;
+
+  @ManyToOne(() => Roles, (role) => role.userRoles)
+  role: Roles;
+
+  @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  updatedAt: Date;
+
+  @DeleteDateColumn({ name: 'deleted_at', nullable: true })
+  deletedAt?: Date;
+}
