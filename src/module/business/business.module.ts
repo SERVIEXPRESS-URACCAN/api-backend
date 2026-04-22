8f6b133856e863a08af513f9a9f1d01273323037
Bryan18-Alvarado <bryanalvarado1904@gmail.com>
2026-04-21 23:51:51 -0600

    feat: agregar City al módulo Business para mejorar la gestión de relaciones

--- src/module/business/business.module.ts
+++ src/module/business/business.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoriesBusiness } from '../categories-business/entities/categories-business.entity';
+import { City } from '../city/entities/city.entity';
 import { Owner } from '../owner/entities/owner.entity';
 import { BusinessController } from './controller/business.controller';
 import { Business } from './entities/business.entity';
 import { BusinessService } from './service/business.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Business, CategoriesBusiness, Owner])],
+  imports: [
+    TypeOrmModule.forFeature([Business, CategoriesBusiness, Owner, City]),
+  ],
   controllers: [BusinessController],
   providers: [BusinessService],
 })
