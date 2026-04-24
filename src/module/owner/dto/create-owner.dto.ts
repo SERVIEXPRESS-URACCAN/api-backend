054365bca7523de533e1f8e149de099ed585c867
Bryan18-Alvarado <bryanalvarado1904@gmail.com>
2026-04-24 16:49:12 -0600

    feat: delete column user into create owner dto

--- src/module/owner/dto/create-owner.dto.ts
+++ src/module/owner/dto/create-owner.dto.ts
@@ -1,12 +1,6 @@
-import { Type } from 'class-transformer';
-import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateOwnerDto {
-  @Type(() => Number)
-  @IsNotEmpty()
-  @IsNumber()
-  user: number;
-
   @IsNotEmpty()
   @IsString()
   razonSocial: string;
