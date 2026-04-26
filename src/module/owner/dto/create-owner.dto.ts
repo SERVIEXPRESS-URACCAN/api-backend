5205d61eda5d82640225410748127b002aa67608
Bryan18-Alvarado <bryanalvarado1904@gmail.com>
2026-04-25 23:42:10 -0600

    feat: add user into createDto

--- src/module/owner/dto/create-owner.dto.ts
+++ src/module/owner/dto/create-owner.dto.ts
@@ -1,6 +1,10 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsInt, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateOwnerDto {
+  @IsInt()
+  @IsNotEmpty()
+  user: number;
+
   @IsNotEmpty()
   @IsString()
   razonSocial: string;
