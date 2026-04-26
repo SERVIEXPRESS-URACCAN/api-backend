d3899bf78f7015d6f06f6965bd1db8bc08c3ba48
katia-silva09 <katiajuliethsilvareyes09@gmail.com>
2026-04-25 23:44:42 -0600

    feat: Implement pagination for roles retrieval and update method names, and autorization for admin

--- src/module/roles/controllers/roles.controller.ts
+++ src/module/roles/controllers/roles.controller.ts
@@ -7,31 +7,38 @@ import {
   ParseIntPipe,
   Patch,
   Post,
+  Query,
 } from '@nestjs/common';
 import { RolesService } from '../services/roles.service';
 import { CreateRolesDto } from '../dto/create-roles.dto';
 import { UpdateRolesDto } from '../dto/update-roles.dto';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { Auth } from 'src/module/auth/decorator/auth.decorator';
 
 @Controller('roles')
 export class RolesController {
   constructor(private readonly rolesService: RolesService) {}
 
   @Get()
-  getAll() {
-    return this.rolesService.getAll();
+  @Auth('admin')
+  getAll(@Query() paginationDto: PaginationDto) {
+    return this.rolesService.findAll(paginationDto);
   }
 
   @Get(':id')
+  @Auth('admin')
   getOne(@Param('id') id: number) {
-    return this.rolesService.getOne(id);
+    return this.rolesService.findOne(id);
   }
 
   @Post()
+  @Auth('admin')
   async create(@Body() createRolesDto: CreateRolesDto) {
     return this.rolesService.create(createRolesDto);
   }
 
   @Patch(':id')
+  @Auth('admin')
   update(
     @Param('id', ParseIntPipe) id: number,
     @Body() updateRolesDto: UpdateRolesDto,
@@ -40,6 +47,7 @@ export class RolesController {
   }
 
   @Delete(':id')
+  @Auth('admin')
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.rolesService.remove(id);
   }
