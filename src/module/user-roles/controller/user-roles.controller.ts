d7912da515084120bf2de82722e3db68334302ac
katia-silva09 <katiajuliethsilvareyes09@gmail.com>
2026-04-25 22:43:05 -0600

    feat: Add pagination and authentication to user role methods

--- src/module/user-roles/controller/user-roles.controller.ts
+++ src/module/user-roles/controller/user-roles.controller.ts
@@ -6,28 +6,35 @@ import {
   Param,
   ParseIntPipe,
   Post,
+  Query,
 } from '@nestjs/common';
 import { AssignRoleDto } from '../dto/user-roles.dto';
 import { UserRolesService } from '../service/user-roles.service';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { Auth } from 'src/module/auth/decorator/auth.decorator';
 
 @Controller('user-roles')
 export class UserRolesController {
   constructor(private readonly userRolesService: UserRolesService) {}
 
   @Post()
+  @Auth('admin')
   assignRole(@Body() dto: AssignRoleDto) {
     return this.userRolesService.assignRole(dto);
   }
-  @Get(':userId/:roleId')
+  @Get('by-user/:userId/:roleId')
+  @Auth('admin')
   findOne(@Param('userId') userId: number, @Param('roleId') roleId: number) {
     return this.userRolesService.findOne(userId, roleId);
   }
   @Get()
-  findAll() {
-    return this.userRolesService.findAll();
+  @Auth('admin')
+  findAll(@Query() paginationDto: PaginationDto) {
+    return this.userRolesService.findAll(paginationDto);
   }
 
   @Delete(':userId/:roleId')
+  @Auth('admin')
   async removeRole(
     @Param('userId', ParseIntPipe) userId: number,
     @Param('roleId', ParseIntPipe) roleId: number,
@@ -35,6 +42,7 @@ export class UserRolesController {
     return this.userRolesService.removeRole({ userId, roleId });
   }
   @Get(':userId')
+  @Auth('admin')
   async getRolesByUser(@Param('userId', ParseIntPipe) userId: number) {
     return this.userRolesService.getRolesByUser(userId);
   }
