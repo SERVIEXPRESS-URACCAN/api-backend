451357741a5f2aecdb51f236890079d4664533a5
Bryan18-Alvarado <bryanalvarado1904@gmail.com>
2026-04-26 00:44:26 -0600

    feat: Implementar paginación en la recuperación de propietarios y métodos de actualización

--- src/module/owner/controller/owner.controller.ts
+++ src/module/owner/controller/owner.controller.ts
@@ -13,12 +13,12 @@ import {
 import { FileFieldsInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { extname } from 'path';
-import { PaginationDto } from 'src/common/dto/pagination.dto';
 import { Auth } from 'src/module/auth/decorator/auth.decorator';
 import { GetUser } from 'src/module/auth/decorator/getUser.decorator';
 import { AuthUser } from 'src/module/auth/interfaces/auth-user.interface';
 import { CreateOwnerDto } from '../dto/create-owner.dto';
 // import { UpdateOwnerDto } from '../dto/update-owner.dto';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
 import { UpdateOwnerDto } from '../dto/update-owner.dto';
 import { OwnerService } from '../service/owner.service';
 
@@ -26,12 +26,29 @@ import { OwnerService } from '../service/owner.service';
 export class OwnerController {
   constructor(private readonly ownerService: OwnerService) {}
 
+  @Get()
+  @Auth('admin')
+  async findAll(@Query() paginationDto: PaginationDto) {
+    const data = await this.ownerService.findAll(paginationDto);
+
+    return {
+      ...data,
+    };
+  }
+
   @Get('me')
   @Auth('owner')
   getMyOwner(@GetUser() user: AuthUser) {
     return this.ownerService.findOne(user.id);
   }
 
+  @Get(':id')
+  @Auth('admin')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.ownerService.findOne(id);
+  }
+
+  @Auth('admin', 'owner')
   @Post()
   @UseInterceptors(
     FileFieldsInterceptor([{ name: 'identificationCardImage', maxCount: 1 }], {
@@ -62,25 +79,31 @@ export class OwnerController {
     };
   }
 
-  @Get()
-  async findAll(
-    @Query() paginationDto: PaginationDto,
+  @Patch('me')
+  @Auth('owner')
+  @UseInterceptors(
+    FileFieldsInterceptor([{ name: 'identificationCardImage', maxCount: 1 }], {
+      storage: diskStorage({
+        destination: './uploads/owners',
+        filename: (req, file, cb) => {
+          const uniqueName =
+            Date.now() + '-' + Math.random().toString(36).substring(2);
+          cb(null, uniqueName + extname(file.originalname));
+        },
+      }),
+    }),
+  )
+  updateOwner(
+    @UploadedFiles()
+    files: { identificationCardImage?: Express.Multer.File[] },
+    @Body() updateOwnerDto: UpdateOwnerDto,
     @GetUser() user: AuthUser,
   ) {
-    const data = await this.ownerService.findAll(paginationDto, user);
-
-    return {
-      ...data,
-    };
-  }
-
-  @Get(':id')
-  @Auth('admin')
-  findOne(@Param('id', ParseIntPipe) id: number) {
-    return this.ownerService.findOne(id);
+    return this.ownerService.update(user.id, updateOwnerDto, files);
   }
 
   @Patch(':id')
+  @Auth('admin')
   @UseInterceptors(
     FileFieldsInterceptor([{ name: 'identificationCardImage', maxCount: 1 }], {
       storage: diskStorage({
