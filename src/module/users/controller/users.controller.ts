c2ed240756e889f9f1e0373bcd73ac82dc094122
katia-silva09 <katiajuliethsilvareyes09@gmail.com>
2026-04-25 20:48:57 -0600

    feat:add autorization admin in post user

--- src/module/users/controller/users.controller.ts
+++ src/module/users/controller/users.controller.ts
@@ -20,6 +20,7 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @Auth('admin')
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
