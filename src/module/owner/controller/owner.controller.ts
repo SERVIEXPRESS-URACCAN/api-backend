27a95704334fd3cd32722caeb7b561a8f3683bd8
Bryan18-Alvarado <bryanalvarado1904@gmail.com>
2026-04-25 23:42:17 -0600

    feat: refactor OwnerController to enhance authorization and clean up unused code

--- src/module/owner/controller/owner.controller.ts
+++ src/module/owner/controller/owner.controller.ts
@@ -4,28 +4,31 @@ import {
   Get,
   Param,
   ParseIntPipe,
-  Patch,
   Post,
   Query,
   UploadedFiles,
-  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
 import { FileFieldsInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { extname } from 'path';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { Auth } from 'src/module/auth/decorator/auth.decorator';
 import { GetUser } from 'src/module/auth/decorator/getUser.decorator';
 import { AuthUser } from 'src/module/auth/interfaces/auth-user.interface';
 import { CreateOwnerDto } from '../dto/create-owner.dto';
-import { UpdateOwnerDto } from '../dto/update-owner.dto';
+// import { UpdateOwnerDto } from '../dto/update-owner.dto';
 import { OwnerService } from '../service/owner.service';
 
-@UseGuards(AuthGuard('jwt'))
 @Controller('owner')
 export class OwnerController {
   constructor(private readonly ownerService: OwnerService) {}
+
+  @Get('me')
+  @Auth('owner')
+  getMyOwner(@GetUser() user: AuthUser) {
+    return this.ownerService.findOne(user.id);
+  }
 
   @Post()
   @UseInterceptors(
@@ -70,31 +73,31 @@ export class OwnerController {
   }
 
   @Get(':id')
-  findOne(@Param('id', ParseIntPipe) id: number, @GetUser() user: AuthUser) {
-    return this.ownerService.findOne(id, user);
+  @Auth('owner')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.ownerService.findOne(id);
   }
 
-  @Patch(':id')
-  @UseInterceptors(
-    FileFieldsInterceptor([{ name: 'identificationCardImage', maxCount: 1 }], {
-      storage: diskStorage({
-        destination: './uploads/owners',
-        filename: (req, file, cb) => {
-          const uniqueName =
-            Date.now() + '-' + Math.random().toString(36).substring(2);
+  // @Patch(':id')
+  // @UseInterceptors(
+  //   FileFieldsInterceptor([{ name: 'identificationCardImage', maxCount: 1 }], {
+  //     storage: diskStorage({
+  //       destination: './uploads/owners',
+  //       filename: (req, file, cb) => {
+  //         const uniqueName =
+  //           Date.now() + '-' + Math.random().toString(36).substring(2);
 
-          cb(null, uniqueName + extname(file.originalname));
-        },
-      }),
-    }),
-  )
-  async update(
-    @Param('id', ParseIntPipe) id: number,
-    @UploadedFiles()
-    files: { identificationCardImage?: Express.Multer.File[] },
-    @Body() updateOwnerDto: UpdateOwnerDto,
-    @GetUser() user: AuthUser,
-  ) {
-    return this.ownerService.update(id, updateOwnerDto, user, files);
-  }
+  //         cb(null, uniqueName + extname(file.originalname));
+  //       },
+  //     }),
+  //   }),
+  // )
+  // async update(
+  //   @Param('id', ParseIntPipe) id: number,
+  //   @UploadedFiles()
+  //   files: { identificationCardImage?: Express.Multer.File[] },
+  //   @Body() updateOwnerDto: UpdateOwnerDto,
+  // ) {
+  //   return this.ownerService.update(id, updateOwnerDto, files);
+  // }
 }
