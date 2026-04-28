a9d9b2a33c5abbb608b3c7506e46679c2c6aad4b
Chow <chowduarte@gmail.com>
2026-04-28 16:20:26 -0600

    feat: Add Motorcycle entity and MandaderoSolicitud services to Mandadero module

--- src/module/mandadero/mandadero.module.ts
+++ src/module/mandadero/mandadero.module.ts
@@ -6,10 +6,18 @@ import { Mandadero } from './entities/mandadero.entity';
 import { User } from '../users/entities/user.entity';
 import { MandaderoPolicyService } from './service/mandadero-policy.service';
 import { MandaderoStatusService } from './service/mandadero-status.service';
+import { MandaderoSolicitudController } from './controller/mandaderoSolicitud/mandadero-solicitud.controller';
+import { MandaderoSolicitudService } from './service/MandaderoSolicitud/Mandadero-solicitud.service';
+import { Motorcycle } from '../motorcycles/entities/motorcycle.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Mandadero, User])],
-  providers: [MandaderoService, MandaderoStatusService, MandaderoPolicyService],
-  controllers: [MandaderoController],
+  imports: [TypeOrmModule.forFeature([Mandadero, User, Motorcycle])],
+  providers: [
+    MandaderoService,
+    MandaderoStatusService,
+    MandaderoPolicyService,
+    MandaderoSolicitudService,
+  ],
+  controllers: [MandaderoController, MandaderoSolicitudController],
 })
 export class MandaderoModule {}
