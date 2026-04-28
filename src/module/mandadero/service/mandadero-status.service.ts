bbbfad401f7402d3549bda39ccf99959438f61e8
Chow <chowduarte@gmail.com>
2026-04-28 17:11:30 -0600

    feat: Mejorar el método reject en MandaderoStatusService para manejar transacciones y actualizar el estado de la motocicleta

--- src/module/mandadero/service/mandadero-status.service.ts
+++ src/module/mandadero/service/mandadero-status.service.ts
@@ -51,18 +51,36 @@ export class MandaderoStatusService {
   }
 
   async reject(id: number) {
-    const mandadero = await this.mandaderoRepository.findOneBy({ id });
-    if (!mandadero) {
-      throw new NotFoundException('Mandadero not found');
+    const queryRunner = this.dataSource.createQueryRunner();
+
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      const mandadero = await this.findMandaderoOrFail(id, queryRunner.manager);
+
+      this.policy.validateRejection(mandadero);
+
+      mandadero.status = ApprovalStatus.REJECTED;
+      mandadero.motorcycle.status = ApprovalStatus.REJECTED;
+
+      mandadero.isActive = false;
+      mandadero.available = false;
+
+      await queryRunner.manager.save(mandadero.motorcycle);
+      await queryRunner.manager.save(mandadero);
+
+      await queryRunner.commitTransaction();
+
+      return {
+        message: 'Mandadero and Motorcycle rejected successfully',
+      };
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      throw error;
+    } finally {
+      await queryRunner.release();
     }
-
-    this.policy.validateRejection(mandadero);
-
-    mandadero.status = ApprovalStatus.REJECTED;
-    mandadero.isActive = false;
-    mandadero.available = false;
-
-    return this.mandaderoRepository.save(mandadero);
   }
 
   async updateMyAvailability(available: boolean, user: AuthUser) {
