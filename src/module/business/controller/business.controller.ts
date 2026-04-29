aefd0bb2008a63d66be31e3f062eaacccf1b94ef
katia-silva09 <katiajuliethsilvareyes09@gmail.com>
2026-04-28 23:35:16 -0600

    feat: validation for owner and admin in the endpoinds

--- src/module/business/controller/business.controller.ts
+++ src/module/business/controller/business.controller.ts
@@ -1,12 +1,10 @@
 import {
   Body,
   Controller,
-  Delete,
   Get,
   Param,
   ParseIntPipe,
   Patch,
-  Post,
   Query,
   UploadedFiles,
   UseInterceptors,
@@ -16,24 +14,64 @@ import { FileFieldsInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { extname } from 'path';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
-import { CreateBusinessDto } from '../dto/create-business.dto';
 import { UpdateBusinessDto } from '../dto/update-business.dto';
 import { BusinessService } from '../service/business.service';
+import { Auth } from 'src/module/auth/decorator/auth.decorator';
+import { GetUser } from 'src/module/auth/decorator/getUser.decorator';
+import { AuthUser } from 'src/module/auth/interfaces/auth-user.interface';
 
 @Controller('business')
 export class BusinessController {
   constructor(private readonly businessService: BusinessService) {}
 
-  @Post()
-  async create(@Body() createBusinessDto: CreateBusinessDto) {
-    const business = await this.businessService.create(createBusinessDto);
+  @Auth('owner')
+  @Get('me')
+  getMyBusiness(@GetUser() user: AuthUser) {
+    return this.businessService.findOne(user.id);
+  }
+
+  @Auth('owner')
+  @Patch('me')
+  @UseInterceptors(
+    FileFieldsInterceptor(
+      [
+        { name: 'logoImage', maxCount: 1 },
+        { name: 'bannerImage', maxCount: 1 },
+      ],
+      {
+        storage: diskStorage({
+          destination: './uploads/business',
+          filename: (req, file, cb) => {
+            const uniqueName =
+              Date.now() + '-' + Math.round(Math.random() * 1e9);
+            const ext = extname(file.originalname);
+            cb(null, `${uniqueName}${ext}`);
+          },
+        }),
+      },
+    ),
+  )
+  async updateMyBusines(
+    @GetUser() user: AuthUser,
+    @Body() updateBusinessDto: UpdateBusinessDto,
+    @UploadedFiles()
+    files: {
+      logoImage?: Express.Multer.File[];
+      bannerImage?: Express.Multer.File[];
+    },
+  ) {
+    const data = await this.businessService.updateMyBusiness(
+      user.id,
+      updateBusinessDto,
+      files,
+    );
 
     return {
-      data: business,
-      message: 'Business creado con éxito',
+      data,
     };
   }
 
+  @Auth('admin')
   @Get()
   async findAll(
     @Query() paginationDto: PaginationDto,
@@ -46,6 +84,7 @@ export class BusinessController {
     };
   }
 
+  @Auth('admin')
   @Get(':id')
   async getOne(@Param('id', ParseIntPipe) id: number) {
     const data = await this.businessService.findOne(id);
@@ -55,6 +94,7 @@ export class BusinessController {
     };
   }
 
+  @Auth('admin')
   @Patch(':id')
   @UseInterceptors(
     FileFieldsInterceptor(
@@ -89,15 +129,6 @@ export class BusinessController {
       updateBusinessDto,
       files,
     );
-
-    return {
-      data,
-    };
-  }
-
-  @Delete(':id')
-  async remove(@Param('id', ParseIntPipe) id: number) {
-    const data = await this.businessService.remove(id);
 
     return {
       data,
