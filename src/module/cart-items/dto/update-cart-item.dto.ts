973e627172d45b68582657cd1f39af2b8c3e2e47
Bryan18-Alvarado <bryanalvarado1904@gmail.com>
2026-05-03 21:11:45 -0600

    feat: redefinir UpdateCartItemDto para incluir validación de cantidad

--- src/module/cart-items/dto/update-cart-item.dto.ts
+++ src/module/cart-items/dto/update-cart-item.dto.ts
@@ -1,4 +1,7 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCartItemDto } from './create-cart-item.dto';
+import { IsInt, Min } from 'class-validator';
 
-export class UpdateCartItemDto extends PartialType(CreateCartItemDto) {}
+export class UpdateCartItemDto {
+  @IsInt()
+  @Min(1)
+  quantity: number;
+}
