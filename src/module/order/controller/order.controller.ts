8dd8efb0046aeb749ae84b5b44eb4d2e5a2bba26
Bryan18-Alvarado <bryanalvarado1904@gmail.com>
2026-05-04 14:40:22 -0600

    feat: refactor OrderController to improve method names and structure

--- src/module/order/controller/order.controller.ts
+++ src/module/order/controller/order.controller.ts
@@ -1,22 +1,50 @@
-import { Controller, Delete, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+
+import { GetUser } from 'src/module/auth/decorator/getUser.decorator';
+import { AuthUser } from 'src/module/auth/interfaces/auth-user.interface';
+
+import { OrderStatus } from '../enum/orderStatus';
 import { OrderService } from '../service/order.service';
 
-@Controller('order')
+@Controller('orders')
 export class OrderController {
   constructor(private readonly orderService: OrderService) {}
 
   @Get()
-  findAll() {
-    return this.orderService.findAll();
+  getMyOrders(@GetUser() user: AuthUser) {
+    return this.orderService.getMyOrders(user.id);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.orderService.findOne(+id);
+  getOrderById(
+    @Param('id', ParseIntPipe) id: number,
+    @GetUser() user: AuthUser,
+  ) {
+    return this.orderService.getOrderById(id, user.id);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.orderService.remove(+id);
+  @Post('checkout/:cartId')
+  checkout(
+    @Param('cartId', ParseIntPipe) cartId: number,
+    @GetUser() user: AuthUser,
+  ) {
+    return this.orderService.createOrderFromCart(user.id, cartId);
+  }
+
+  @Patch(':id/status')
+  updateStatus(
+    @Param('id', ParseIntPipe) id: number,
+    @Body('status') status: OrderStatus,
+    @GetUser() user: AuthUser,
+  ) {
+    return this.orderService.updateStatus(id, status, user);
   }
 }
