f1e46d663e82009ab996fed30d9bd4839a3a4fe5
Bryan18-Alvarado <bryanalvarado1904@gmail.com>
2026-05-04 14:40:51 -0600

    feat: update OrderModule to include OrderItem and Cart in TypeOrmModule imports

--- src/module/order/order.module.ts
+++ src/module/order/order.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Cart } from '../cart/entities/cart.entity';
+import { OrderItem } from '../order-items/entities/order-item.entity';
 import { OrderController } from './controller/order.controller';
 import { Order } from './entities/order.entity';
 import { OrderService } from './service/order.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order])],
+  imports: [TypeOrmModule.forFeature([Order, OrderItem, Cart])],
   controllers: [OrderController],
   providers: [OrderService],
 })
