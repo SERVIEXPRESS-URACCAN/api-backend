9acd424765e88931f4831b6594fe9db2901f2f42
Bryan18-Alvarado <bryanalvarado1904@gmail.com>
2026-05-03 19:44:27 -0600

    feat: define CartItem entity and establish relationship with Cart

--- src/module/cart-items/entities/cart-item.entity.ts
+++ src/module/cart-items/entities/cart-item.entity.ts
@@ -1,1 +1,33 @@
-export class CartItem {}
+import { Cart } from 'src/module/cart/entities/cart.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  Index,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Index(['cartId', 'productId'], { unique: true })
+@Entity()
+export class CartItem {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  cartId: number;
+
+  @Column()
+  productId: number;
+
+  @Column({ type: 'int', default: 1 })
+  quantity: number;
+
+  @ManyToOne(() => Cart, (cart) => cart.items, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'cartId' })
+  cart: Cart;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
