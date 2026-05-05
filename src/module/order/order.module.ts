5628b945a455993e9cb805ec5cff3c322f49f04d
Chow <chowduarte@gmail.com>
2026-05-04 18:14:48 -0600

    feat: agregar Business a las importaciones del módulo de pedidos

--- src/module/order/order.module.ts
+++ src/module/order/order.module.ts
@@ -5,9 +5,14 @@ import { OrderItem } from '../order-items/entities/order-item.entity';
 import { OrderController } from './controller/order.controller';
 import { Order } from './entities/order.entity';
 import { OrderService } from './service/order.service';
+import { BusinessModule } from '../business/business.module';
+import { Business } from '../business/entities/business.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order, OrderItem, Cart])],
+  imports: [
+    TypeOrmModule.forFeature([Order, OrderItem, Cart, Business]),
+    BusinessModule,
+  ],
   controllers: [OrderController],
   providers: [OrderService],
 })
