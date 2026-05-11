5d221d0c39ab04777d7fa31f2e6c540d4bf6fe7d
Bryan18-Alvarado <bryanalvarado1904@gmail.com>
2026-05-11 12:24:51 -0600

    feat: Add IsInt validation to pagination fields

--- src/common/dto/pagination.dto.ts
+++ src/common/dto/pagination.dto.ts
@@ -1,19 +1,22 @@
 import { Type } from 'class-transformer';
-import { IsNumber, IsOptional, Min } from 'class-validator';
+import { IsInt, IsNumber, IsOptional, Min } from 'class-validator';
 
 export class PaginationDto {
   @IsOptional()
   @Type(() => Number)
+  @IsInt()
   @Min(1)
   page?: number = 1;
 
   @IsOptional()
   @Type(() => Number)
+  @IsInt()
   @Min(1)
   limit?: number = 10;
 
   @IsOptional()
   @Type(() => Number)
   @IsNumber()
+  @IsInt()
   city?: number;
 }
