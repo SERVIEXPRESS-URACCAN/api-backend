612bef281405cfb44c7f6dce287d42bd4f8db9d6
Bryan18-Alvarado <bryanalvarado1904@gmail.com>
2026-05-27 23:36:03 -0600

    feat: actualizar servicios y DTOs para incluir validación de género en Owner y Profile

--- src/module/profile/dto/update-profile.dto.ts
+++ src/module/profile/dto/update-profile.dto.ts
@@ -1,4 +1,21 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateProfileDto } from './profile.dto';
+import { Type } from 'class-transformer';
+import { IsNumber, IsOptional, IsString } from 'class-validator';
 
-export class UpdateProfileDto extends PartialType(CreateProfileDto) {}
+export class UpdateProfileDto {
+  @IsOptional()
+  @IsString()
+  name?: string;
+
+  @IsOptional()
+  @IsString()
+  lastName?: string;
+
+  @IsOptional()
+  @IsString()
+  cellphone?: string;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsNumber()
+  genderId?: number;
+}
