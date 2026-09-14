4657b083c48acfda71f7b527e82d59572c9318c5
harrychow012 <chowduarte@gmail.com>
2026-09-14 15:35:05 -0600

    feat: agregar módulo Dashboard al módulo principal

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,6 +22,7 @@ import { UserRolesModule } from './module/user-roles/user-roles.module';
 import { UsersModule } from './module/users/users.module';
 import { OrderItemsModule } from './module/order-items/order-items.module';
 import { ScheduleModule } from '@nestjs/schedule';
+import { DashboardModule } from './dashboard/dashboard.module';
 
 @Module({
   imports: [
@@ -65,6 +66,7 @@ import { ScheduleModule } from '@nestjs/schedule';
     CartItemsModule,
     OrderModule,
     OrderItemsModule,
+    DashboardModule,
   ],
   controllers: [AppController],
   providers: [AppService],
